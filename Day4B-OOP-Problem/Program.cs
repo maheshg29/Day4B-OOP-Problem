3521af77036fbb7cb5dac7426bdc57bbb37f3ad9
mahesh <gangurdemahesh9@gmail.com>
2023-03-22 23:48:48 +0530

    [mahesh]add program file for Rectangle Star Pattern

--- Day4B-OOP-Problem/Program.cs
+++ Day4B-OOP-Problem/Program.cs
@@ -13,7 +13,7 @@ namespace Day4B_OOP_Problem
             Console.WriteLine("Welcome To Day 4B- Object Orianted Problem");
             Console.WriteLine("Please Select any one option \n 1. Count of total number of duplicate elements\n" +
                 " 2. To find Unique Element\n 3.count the frequency of each element \n 4. maximum and minimum element \n " +
-                "5. Right Angle Star Pattern");
+                "5. Right Angle Star Pattern \n 6. Rectangle Star Pattern");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -33,6 +33,9 @@ namespace Day4B_OOP_Problem
                     break;
                 case 5:
                     StarPattern.RightAngleTrangle();
+                    break;
+                case 6:
+                    StarPattern.RectangleStarPattern();
                     break;
                 default:
                     Console.WriteLine("Please Select Right option from Given");
--- Day4B-OOP-Problem/StarPattern.cs
+++ Day4B-OOP-Problem/StarPattern.cs
@@ -23,5 +23,34 @@ namespace Day4B_OOP_Problem
                 Console.WriteLine();
             }
         }
+
+        public static void RectangleStarPattern()
+        {
+            Console.WriteLine("Please Enter Number for How much lines you want in Pattern");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            for (int i = rows; i >= 1; i--)
+            {
+                if (i == rows || i == 1)
+                {
+                    for (int j = 0; j <= rows; j++)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+                for (int j = 1; j <= rows; j++)
+                {
+                    
+                    if (j==1 || j == rows)
+                    Console.Write("*");
+                    else 
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine();
+            }
+
+
+        }
     }
 }
