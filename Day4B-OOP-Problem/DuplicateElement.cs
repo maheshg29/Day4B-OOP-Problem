070c23c90737a8de165417531ac7d050baf4a920
mahesh <gangurdemahesh9@gmail.com>
2023-03-22 11:09:49 +0530

    [mahesh]add Program file for Count of Duplicate Element is Given String

---
+++ Day4B-OOP-Problem/DuplicateElement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4B_OOP_Problem
+{
+    public class DuplicateElement
+    {
+        public static void DuplicateIntElement()
+        {
+            Console.WriteLine("Enter how many element you want to Put in Array");
+            int size=Convert.ToInt32(Console.ReadLine());
+            int[] intArray=new int[size];
+            int count=0;
+            Console.WriteLine("Enter one by one element ");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Your Enter Element is \n");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.Write(intArray[i]+" ");
+            }
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                for (int j = i + 1; j < intArray.Length; j++)
+                {
+                    if (intArray[i] == intArray[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("\n The Duplicate Number in Given string is "+count);
+        }
+    }
+}
--- Day4B-OOP-Problem/Program.cs
+++ Day4B-OOP-Problem/Program.cs
@@ -11,7 +11,18 @@ namespace Day4B_OOP_Problem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Day 4B- Object Orianted Problem");
+            Console.WriteLine("Please Select any one option \n 1. Count of total number of duplicate elements");
+            int option = Convert.ToInt32(Console.ReadLine());
 
+            switch (option)
+            {
+                case 1:
+                    DuplicateElement.DuplicateIntElement();
+                    break;
+                default:
+                    Console.WriteLine("Please Select Right option from Given");
+                    break;
+            }
         }
     }
 }
